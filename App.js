ae2218539b1c2da9311b34a36f1831943688c784
nhuvy34289 <danghnh@dipro.vn>
2023-10-24 12:38:41 +0700

    hhhh

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start</Text>
+      <Text>Open up App.js to</Text>
       <StatusBar style="auto" />
     </View>
   );
